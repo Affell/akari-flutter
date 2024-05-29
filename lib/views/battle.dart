66e7cf98c51d4cabcbadabbbf66adde277dfa268
Axel Messaoudi <axel.messaoudi.p@gmail.com>
2024-05-30 00:07:29 +0200

    animation recherche

--- lib/views/battle.dart
+++ lib/views/battle.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class Battle extends StatelessWidget {
-  const Battle({super.key});
+class Battle extends StatefulWidget {
+  const Battle({Key? key}) : super(key: key);
+
+  @override
+  _BattleState createState() => _BattleState();
+}
+
+class _BattleState extends State<Battle> {
+  String _searchingText = 'Recherche d\'adversaire';
+  int _dotCount = 0;
+  late Timer _timer;
+  bool _isSearching = false;
+
+  void _startAnimation() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _dotCount = (_dotCount + 1) % 4;
+        _searchingText = 'Recherche d\'adversaire' + '.' * _dotCount;
+      });
+    });
+  }
+
+  void _stopAnimation() {
+    _timer.cancel();
+    setState(() {
+      _isSearching = false;
+      _searchingText = 'Recherche d\'adversaire';
+    });
+  }
+
+  @override
+  void dispose() {
+    if (_timer.isActive) {
+      _timer.cancel();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +45,40 @@ class Battle extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Battle 1v1'),
       ),
-      body: const MaterialApp(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_isSearching)
+              Text(
+                _searchingText,
+                style: const TextStyle(fontSize: 24),
+              ),
+            const SizedBox(height: 20),
+            if (!_isSearching)
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _isSearching = true;
+                    _startAnimation();
+                  });
+                },
+                child: const Text('Lancer la recherche'),
+              ),
+            if (_isSearching)
+              ElevatedButton(
+                onPressed: _stopAnimation,
+                child: const Text('Arrêter'),
+              ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Battle(),
+  ));
 }
